c66290c1b514d65d51624205add3a7938cabb510
MMeow223 <elviswong2002@hotmail.com>
2023-10-03 12:02:36 +0800

    Commit

---
+++ greenroom_unit.ino
@@ -0,0 +1,30 @@
+// constant variables here
+const int PIN_TEMP_HUMDIT = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// create new function here for each module
+
+void plant_monitoring_loop(){
+
+}
+
+void environment_monitoring_loop(){
+
+}
+
+void nutrient_loading_loop(){
+
+}
+
+void environmnet_control_loop(){
+
+}
