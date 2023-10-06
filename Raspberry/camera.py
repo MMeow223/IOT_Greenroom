1ae0b12700b7a05c9fcd5d90a06053a5276d7b11
MMeow223 <elviswong2002@hotmail.com>
2023-10-06 15:37:15 +0800

    Update camera.py

--- Raspberry/camera.py
+++ Raspberry/camera.py
@@ -1,15 +1,30 @@
-from picamera import PiCamera
-from time import sleep
-import datetime
-camera = PiCamera()
+# from picamera import PiCamera
+# from time import sleep
+# import datetime
+# camera = PiCamera()
 
-camera.start_preview(alpha=200)
-sleep(5)
-camera.stop_preview()
+# camera.start_preview(alpha=200)
+# sleep(5)
+# camera.stop_preview()
 
-def capture_image():
-    image_filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S") 
-    camera.capture('/home/pi/Desktop/{0}.jpg'.format(image_filename))
+# def capture_image():
+#     image_filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S") 
+#     camera.capture('/home/pi/Desktop/{0}.jpg'.format(image_filename))
     
 
     
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    check, frame = cam.read()
+
+    cv2.imshow('video', frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
