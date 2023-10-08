a928a5b2252c2b9621d1a4bb065df94dd495e3f0
MMeow223 <elviswong2002@hotmail.com>
2023-10-08 23:45:44 +0800

    Add store image to firebase functionality

--- Raspberry/app.py
+++ Raspberry/app.py
@@ -3,10 +3,15 @@
 
 # Arduino sends data to Raspberry Pi via serial communication
 # Raspberry Pi sends data to AWS IoT Core via MQTT
-import serial
+# import serial
 import os
 from dotenv import load_dotenv
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+# from firebase import firebase
+import firebase_admin
+from firebase_admin import credentials, storage
+
+from camera import capture_image
 
 load_dotenv()
 
@@ -18,25 +23,67 @@ AWS_PRIVATE_KEY = os.getenv("AWS_PRIVATE_KEY")
 AWS_CERTIFICATE = os.getenv("AWS_CERTIFICATE")
 PORT = os.getenv("PORT")
 BAUDRATE = os.getenv("BAUDRATE")
+CREDENTIALS = os.getenv("CREDENTIALS")
+FIREBASE_STORAGE = os.getenv("FIREBASE_STORAGE")
 
 DICT = {
-    "1001": 1, 
-    "1002": 0, 
-    }
+    "1001": 1,
+    "1002": 0,
+}
 
 myMQTTClient = AWSIoTMQTTClient(AWS_CLIENT)
 
 
+def firebase_storage(bucket_dir, image_path):
+
+    cred = credentials.Certificate(CREDENTIALS)
+    # print("Uploading image to Firebase Storage...")
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': FIREBASE_STORAGE
+    })
+
+    # Path to the image you want to upload
+    # image_path = "image/2023-10-08-01115008-62db-4593-827a-24c3927a0ff9.png"
+
+    filename = os.path.basename(image_path)
+
+    # Create a Cloud Storage client
+    bucket = storage.bucket()
+
+    # Define the destination path in Firebase Storage (optional)
+    destination_blob_name = "{}/{}".format(bucket_dir, filename)
+
+    # Upload the image
+    try:
+        # Open and read the image
+        with open(image_path, "rb") as image_file:
+            image_data = image_file.read()
+
+        # Create a blob and upload the image data
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_string(image_data, content_type="image/jpeg")
+
+        print(
+            f"Image {image_path} uploaded to Firebase Storage at {destination_blob_name}")
+    except Exception as e:
+        print(f"Error uploading image: {e}")
+
+    # Clean up: Delete the temporary credentials
+    firebase_admin.delete_app(firebase_admin.get_app())
+
+    os.remove(image_path)
+
 
 def arduino_connection():
     arduino = serial.Serial(PORT, BAUDRATE)
-    
+
 
 def aws_iot_connection():
 
     # AWS IoT certificate based connection
     myMQTTClient.configureEndpoint(AWS_ENDPOINT, AWS_PORT)
-    myMQTTClient.configureCredentials(AWS_ROOT_CA, AWS_PRIVATE_KEY, AWS_CERTIFICATE)
+    myMQTTClient.configureCredentials(
+        AWS_ROOT_CA, AWS_PRIVATE_KEY, AWS_CERTIFICATE)
     # Infinite offline Publish queueing
     myMQTTClient.configureOfflinePublishQueueing(-1)
     myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
@@ -44,7 +91,7 @@ def aws_iot_connection():
     myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
     # connect and publish
     myMQTTClient.connect()
-    
+
 
 def decode(data):
     pass
@@ -53,18 +100,24 @@ def decode(data):
 def encode(data):
     pass
 
+
 def publish_topic():
     pass
+
 
 def subscribe_topic():
     myMQTTClient.subscribe(topic_name, 1, subscription_callback)
     pass
 
+
 def main():
-    while True:
-        data = arduino.readline()
-        print(data)
+    image_path = capture_image()
+    firebase_storage("plant_size_image",
+                     image_path)
+    # while True:
+    #     data = arduino.readline()
+    #     print(data)
 
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     main()
