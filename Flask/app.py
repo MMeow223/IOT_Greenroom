ca8b0de62df6f12c49b9786693e58c0d155020a1
MMeow223 <elviswong2002@hotmail.com>
2023-10-26 02:54:27 +0800

    Adding Charts and Improve UI on Dashboard

--- Flask/app.py
+++ Flask/app.py
@@ -1,55 +1,93 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template
 import paho.mqtt.client as mqtt
 from apscheduler.schedulers.background import BackgroundScheduler
 import os
 import json
+from decimal import Decimal
+from datetime import datetime
 
 # Currently this connects to local db
 import aws_rds_com as db
 
+from aws_rds_com import *
+
 app = Flask(__name__)
 
 # IP address may change each time broker is restarted
-MQTT_SERVER = "172.20.10.4"
-actuator_topic = 'actuator'
-scheduler_topic = "scheduler"
-sensor_topic = "sensor"
-port = 5000
+# MQTT_SERVER = "172.20.10.4"
+# actuator_topic = 'actuator'
+# scheduler_topic = "scheduler"
+# sensor_topic = "sensor"
+# port = 5000
 
-def on_connect(client, userdata, flags, rc):
-    client.publish(actuator_topic, "STARTING SERVER")
-    client.publish(actuator_topic, "CONNECTED")
-    client.subscribe(sensor_topic)
-
-
-def on_message(client, userdata, msg):
-    payload = str(msg.payload, encoding='utf-8')
-    print(payload)
-    splitStr = payload.split(":")
-    print(splitStr)
-    secondSplit = splitStr[1].split(";")
-    print(secondSplit)
-    match splitStr[0]:
-        case "Water":
-            db.insert_sensor_by_type("moisture", secondSplit[0], secondSplit[1])
-        case "Light Value":
-            db.insert_sensor_by_type("light", secondSplit[0], secondSplit[1])
-        case "Temperature":
-            db.insert_sensor_by_type("temperature", secondSplit[0], secondSplit[1])
-        case "level":
-            db.insert_sensor_by_type("level", secondSplit[0], secondSplit[1])
-        case "Growth Light":
-            db.insert_actuator_by_type("light", secondSplit[0], secondSplit[1])
-        case "Water Pump":
-            db.insert_actuator_by_type("water", secondSplit[0], secondSplit[1])
-        case "Fan":
-            db.insert_actuator_by_type("fan", secondSplit[0], secondSplit[1])
+# def on_connect(client, userdata, flags, rc):
+#     client.publish(actuator_topic, "STARTING SERVER")
+#     client.publish(actuator_topic, "CONNECTED")
+#     client.subscribe(sensor_topic)
 
 
-@app.route('/', methods=['GET', 'POST'])
+# def on_message(client, userdata, msg):
+#     payload = str(msg.payload, encoding='utf-8')
+#     print(payload)
+#     splitStr = payload.split(":")
+#     print(splitStr)
+#     secondSplit = splitStr[1].split(";")
+#     print(secondSplit)
+#     match splitStr[0]:
+#         case "Water":
+#             db.insert_sensor_by_type("moisture", secondSplit[0], secondSplit[1])
+#         case "Light Value":
+#             db.insert_sensor_by_type("light", secondSplit[0], secondSplit[1])
+#         case "Temperature":
+#             db.insert_sensor_by_type("temperature", secondSplit[0], secondSplit[1])
+#         case "level":
+#             db.insert_sensor_by_type("level", secondSplit[0], secondSplit[1])
+#         case "Growth Light":
+#             db.insert_actuator_by_type("light", secondSplit[0], secondSplit[1])
+#         case "Water Pump":
+#             db.insert_actuator_by_type("water", secondSplit[0], secondSplit[1])
+#         case "Fan":
+#             db.insert_actuator_by_type("fan", secondSplit[0], secondSplit[1])
+
+@app.route('/')
 def index():
-    greenroom = db.init_db()
-    return render_template('index.html', greenroom=greenroom)
+    
+    greenrooms= get_greenroom_all()
+    for gr in greenrooms:
+        soil_chart_label = []
+        light_chart_label = []
+        temperature_chart_label = []
+        soil_chart_data = []
+        light_chart_data = []
+        temperature_chart_data = []
+        
+        for i in get_record_greenroom(gr["greenroom_id"],True,"soil_moisture"):
+            soil_chart_label.append(i["timestamp"])
+            soil_chart_data.append(i["value"])
+        
+        for i in get_record_greenroom(gr["greenroom_id"],True,"light"):
+            light_chart_label.append(i["timestamp"])
+            light_chart_data.append(i["value"])
+            
+        for i in get_record_greenroom(gr["greenroom_id"],True,"temperature"):
+            temperature_chart_label.append(i["timestamp"])
+            temperature_chart_data.append(i["value"])
+            
+        greenrooms[greenrooms.index(gr)]["soil_chart_data"] = soil_chart_data
+        greenrooms[greenrooms.index(gr)]["light_chart_data"] = light_chart_data
+        greenrooms[greenrooms.index(gr)]["temperature_chart_data"] = temperature_chart_data
+        
+        greenrooms[greenrooms.index(gr)]["soil_chart_label"] = soil_chart_label
+        greenrooms[greenrooms.index(gr)]["light_chart_label"] = light_chart_label
+        greenrooms[greenrooms.index(gr)]["temperature_chart_label"] = temperature_chart_label
+        
+        
+    data_template = {
+        "greenroom": greenrooms,
+        
+    }
+
+    return render_template('index.html', data=data_template)
 
 @app.route('/greenroom-detail/<id>', methods=['GET', 'POST'])
 def page_greenroom_detail(id):
@@ -66,41 +104,41 @@ def page_greenroom_detail(id):
     greenroom = db.select_by_id(id)
     return render_template('greenroom-detail.html', greenroom=greenroom)
 
-@app.route('/manual', methods=['GET', 'POST'])
-# Template for publishing manual control messages through MQTT
-def manual_control():
-    # Defines all the name of "modules" to loop through (the name should match the "name" attr in form)
-    all_param = ["nutrient", "light"]
-    for param in all_param:
-        # Get the posted value
-        value = request.form.get(param)
-        if value != None:
-            msg = f"{param}:{value}"
-            print(msg)
-            client.publish(actuator_topic, msg)
+# @app.route('/manual', methods=['GET', 'POST'])
+# # Template for publishing manual control messages through MQTT
+# def manual_control():
+#     # Defines all the name of "modules" to loop through (the name should match the "name" attr in form)
+#     all_param = ["nutrient", "light"]
+#     for param in all_param:
+#         # Get the posted value
+#         value = request.form.get(param)
+#         if value != None:
+#             msg = f"{param}:{value}"
+#             print(msg)
+#             client.publish(actuator_topic, msg)
 
-    return render_template('manual_control.html')
+#     return render_template('manual_control.html')
 
 @app.route('/analysis')
 def analysis():
     return render_template('analysis.html')
 
-def scheduler_read_sensor():
-    print("Publishing to read sensor data...")
-    client.publish(scheduler_topic, "read")
+# def scheduler_read_sensor():
+#     print("Publishing to read sensor data...")
+#     client.publish(scheduler_topic, "read")
 
 if __name__ == '__main__':
-    client = mqtt.Client()
-    #client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(MQTT_SERVER)
-    client.loop_start()
+    # client = mqtt.Client()
+    # #client.username_pw_set(username, password)
+    # client.on_connect = on_connect
+    # client.on_message = on_message
+    # client.connect(MQTT_SERVER)
+    # client.loop_start()
 
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(scheduler_read_sensor, 'interval', minutes=15)
-    scheduler.start()
+    # scheduler = BackgroundScheduler()
+    # scheduler.add_job(scheduler_read_sensor, 'interval', minutes=15)
+    # scheduler.start()
 
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
 
     
--- Flask/aws_rds_com.py
+++ Flask/aws_rds_com.py
@@ -1,119 +1,221 @@
 import mysql.connector
 from datetime import datetime, timedelta
 
+# CONSTANT
+TABLE_CONVERSION = {
+    'temperature': 'temperature_sensor',
+    'light': 'light_sensor',
+    'air_moisture': 'air_moisture_sensor',
+    'soil_moisture': 'soil_moisture_sensor',
+    'level': 'water_level_sensor'
+}
+
+
 def connect_db():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
-        database="iot_launcher"
+        database="iot_greenroom"
     )
-
     return connection
 
-def select_by_id(id:int):
+
+def get_record_all(type = { "soil_moisture", "water_level", "air_moisture", "light", "temperature"}):
     conn = connect_db()
     cursor = conn.cursor()
-
-    sql = "SELECT name FROM greenroom WHERE id = %s LIMIT 1"
-    param = (id,)
-    cursor.execute(sql, param)
-    result = cursor.fetchone()
-
-    gr = {
-        "id" : id,
-        "name" : result[0]
-    }
-
-    sql = "SELECT reading,time FROM sensor WHERE type = 'level' AND greenroom = %s ORDER BY time DESC LIMIT 1"
-    param = (id,)
-    cursor.execute(sql, param)
-    result = cursor.fetchone()
-    if result[0] == "0":
-        gr["nutrientLow"] = True
-    else:
-        gr["nutrientLow"] = False
-
-    sql = "SELECT reading,time FROM sensor WHERE type = 'light' AND greenroom = %s ORDER BY time DESC LIMIT 1"
-    param = (id,)
-    cursor.execute(sql, param)
-    result = cursor.fetchone()
-    gr["light"] = result[0]
-
-    sql = "SELECT reading,time FROM sensor WHERE type = 'moisture' AND greenroom = %s ORDER BY time DESC LIMIT 1"
-    param = (id,)
-    cursor.execute(sql, param)
-    result = cursor.fetchone()
-    gr["moisture"] = result[0]
-
-    sql = "SELECT reading,time FROM sensor WHERE type = 'temperature' AND greenroom = %s ORDER BY time DESC LIMIT 1"
-    param = (id,)
-    cursor.execute(sql, param)
-    result = cursor.fetchone()
-    gr["temperature"] = result[0]
-
-    sql = "SELECT status,time FROM actuator WHERE type = 'nutrient' AND greenroom = %s ORDER BY time DESC LIMIT 1"
-    param = (id,)
-    cursor.execute(sql, param)
-    result = cursor.fetchone()
-    gr["nutrientStatus"] = result[0]
-    if(result[0] == "1"):
-        gr["nutrientTime"] = result[1] + timedelta(minutes=5)
-    else:
-        gr["nutrientTime"] = result[1]
+    sql = "SELECT * FROM `"+TABLE_CONVERSION[type]+"`"
     
-
-    sql = "SELECT status,time FROM actuator WHERE type = 'light' AND greenroom = %s ORDER BY time DESC LIMIT 1"
-    param = (id,)
-    cursor.execute(sql, param)
-    result = cursor.fetchone()
-    gr["lightStatus"] = result[0]
-    gr["lightTime"] = result[1]
-
-    sql = "SELECT status,time FROM actuator WHERE type = 'water' AND greenroom = %s ORDER BY time DESC LIMIT 1"
-    param = (id,)
-    cursor.execute(sql, param)
-    result = cursor.fetchone()
-    gr["waterStatus"] = result[0]
-    if(result[0] == "1"):
-        gr["waterTime"] = result[1] + timedelta(minutes=4)
-    else:
-        gr["waterTime"] = result[1]
-
-    sql = "SELECT status,time FROM actuator WHERE type = 'fan' AND greenroom = %s ORDER BY time DESC LIMIT 1"
-    param = (id,)
-    cursor.execute(sql, param)
-    result = cursor.fetchone()
-    gr["fanStatus"] = result[0]
-    gr["fanTime"] = result[1]
-
-    return gr
-
-def init_db():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT id FROM greenroom")
+    cursor.execute(sql)
+    # cursor.execute(sql, param)
     result = cursor.fetchall()
     
-    greenroom = []
-    for r in result:
-        gr = select_by_id(r[0])
-        greenroom.append(gr)
+    result_list = []
+    for row in result:
+        result_dict = {
+            "id": row[0],
+            "value": str(row[1]),  # Convert Decimal to string
+            "timestamp": row[2].strftime('%Y-%m-%d %H:%M:%S'),  # Format datetime as string
+            "greenroom_id": row[3]
+        }
+        result_list.append(result_dict)
+        
+    return result_list
 
-    return greenroom
-
-def insert_sensor_by_type(type:str, reading:str, id:int):
+def get_record_greenroom(greenroom_id, today=False, type = { "soil_moisture", "water_level", "air_moisture", "light", "temperature"}):
     conn = connect_db()
     cursor = conn.cursor()
-    sql = "INSERT INTO sensor (type,reading,greenroom) VALUES (%s,%s,%s)"
-    param = (type, reading, id,)
+    sql = "SELECT * FROM `"+TABLE_CONVERSION[type]+"` WHERE greenroom_id = %s AND timestamp >= %s"
+    
+    if today:
+        # equal to 0:00:00 today
+        date = datetime.now() - timedelta(days=1)
+        # now = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    else:
+        # start from year 2023
+        date = datetime(2023, 1, 1)
+        
+    param = (greenroom_id, date,)
     cursor.execute(sql, param)
-    conn.commit()
+    result = cursor.fetchall()
+    
+    result_list = []
+    for row in result:
+        result_dict = {
+            "id": row[0],
+            "value": str(row[1]),  # Convert Decimal to string
+            "timestamp": row[2].strftime('%Y-%m-%d %H:%M:%S'),  # Format datetime as string
+            "greenroom_id": row[3]
+        }
+        result_list.append(result_dict)
+        
+    return result_list
 
-def insert_actuator_by_type(type:str, status:str, id:int):
+def get_record_date(date, type = { "soil_moisture", "water_level", "air_moisture", "light", "temperature"}):
     conn = connect_db()
     cursor = conn.cursor()
-    sql = "INSERT INTO actuator (type,status,greenroom) VALUES (%s,%s,%s)"
-    param = (type, status, id,)
+    sql = "SELECT * FROM `"+TABLE_CONVERSION[type]+"`  WHERE timestamp = %s"
+    param = (date,)
     cursor.execute(sql, param)
-    conn.commit()
+    result = cursor.fetchall()
+    
+    result_list = []
+    for row in result:
+        result_dict = {
+            "id": row[0],
+            "value": str(row[1]),  # Convert Decimal to string
+            "timestamp": row[2].strftime('%Y-%m-%d %H:%M:%S'),  # Format datetime as string
+            "greenroom_id": row[3]
+        }
+        result_list.append(result_dict)
+        
+    return result_list
+
+def get_greenroom_all():
+    conn = connect_db()
+    cursor = conn.cursor()
+    sql = "SELECT * FROM greenroom"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    
+    result_list = []
+    for row in result:
+        result_dict = {
+            "greenroom_id": row[0],
+            "name": row[1],  # Convert Decimal to string
+            "location": row[2],  # Format datetime as string
+            "desrciption": row[3]
+        }
+        result_list.append(result_dict)
+        
+    return result_list
+
+# def select_by_id(id:int):
+#     conn = connect_db()
+#     cursor = conn.cursor()
+
+#     sql = "SELECT name FROM greenroom WHERE id = %s LIMIT 1"
+#     param = (id,)
+#     cursor.execute(sql, param)
+#     result = cursor.fetchone()
+
+#     gr = {
+#         "id" : id,
+#         "name" : result[0]
+#     }
+
+#     sql = "SELECT reading,time FROM sensor WHERE type = 'level' AND greenroom = %s ORDER BY time DESC LIMIT 1"
+#     param = (id,)
+#     cursor.execute(sql, param)
+#     result = cursor.fetchone()
+#     if result[0] == "0":
+#         gr["nutrientLow"] = True
+#     else:
+#         gr["nutrientLow"] = False
+
+#     sql = "SELECT reading,time FROM sensor WHERE type = 'light' AND greenroom = %s ORDER BY time DESC LIMIT 1"
+#     param = (id,)
+#     cursor.execute(sql, param)
+#     result = cursor.fetchone()
+#     gr["light"] = result[0]
+
+#     sql = "SELECT reading,time FROM sensor WHERE type = 'moisture' AND greenroom = %s ORDER BY time DESC LIMIT 1"
+#     param = (id,)
+#     cursor.execute(sql, param)
+#     result = cursor.fetchone()
+#     gr["moisture"] = result[0]
+
+#     sql = "SELECT reading,time FROM sensor WHERE type = 'temperature' AND greenroom = %s ORDER BY time DESC LIMIT 1"
+#     param = (id,)
+#     cursor.execute(sql, param)
+#     result = cursor.fetchone()
+#     gr["temperature"] = result[0]
+
+#     sql = "SELECT status,time FROM actuator WHERE type = 'nutrient' AND greenroom = %s ORDER BY time DESC LIMIT 1"
+#     param = (id,)
+#     cursor.execute(sql, param)
+#     result = cursor.fetchone()
+#     gr["nutrientStatus"] = result[0]
+#     if(result[0] == "1"):
+#         gr["nutrientTime"] = result[1] + timedelta(minutes=5)
+#     else:
+#         gr["nutrientTime"] = result[1]
+    
+
+#     sql = "SELECT status,time FROM actuator WHERE type = 'light' AND greenroom = %s ORDER BY time DESC LIMIT 1"
+#     param = (id,)
+#     cursor.execute(sql, param)
+#     result = cursor.fetchone()
+#     gr["lightStatus"] = result[0]
+#     gr["lightTime"] = result[1]
+
+#     sql = "SELECT status,time FROM actuator WHERE type = 'water' AND greenroom = %s ORDER BY time DESC LIMIT 1"
+#     param = (id,)
+#     cursor.execute(sql, param)
+#     result = cursor.fetchone()
+#     gr["waterStatus"] = result[0]
+#     if(result[0] == "1"):
+#         gr["waterTime"] = result[1] + timedelta(minutes=4)
+#     else:
+#         gr["waterTime"] = result[1]
+
+#     sql = "SELECT status,time FROM actuator WHERE type = 'fan' AND greenroom = %s ORDER BY time DESC LIMIT 1"
+#     param = (id,)
+#     cursor.execute(sql, param)
+#     result = cursor.fetchone()
+#     gr["fanStatus"] = result[0]
+#     gr["fanTime"] = result[1]
+
+#     return gr
+
+# def init_db():
+#     conn = connect_db()
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT id FROM greenroom")
+#     result = cursor.fetchall()
+    
+#     greenroom = []
+#     for r in result:
+#         gr = select_by_id(r[0])
+#         greenroom.append(gr)
+        
+#     print(greenroom)
+
+#     return greenroom
+
+# def insert_sensor_by_type(type:str, reading:str, id:int):
+#     conn = connect_db()
+#     cursor = conn.cursor()
+#     sql = "INSERT INTO sensor (type,reading,greenroom) VALUES (%s,%s,%s)"
+#     param = (type, reading, id,)
+#     cursor.execute(sql, param)
+#     conn.commit()
+
+# def insert_actuator_by_type(type:str, status:str, id:int):
+#     conn = connect_db()
+#     cursor = conn.cursor()
+#     sql = "INSERT INTO actuator (type,status,greenroom) VALUES (%s,%s,%s)"
+#     param = (type, status, id,)
+#     cursor.execute(sql, param)
+#     conn.commit()
+    
